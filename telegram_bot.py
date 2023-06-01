d63056efafe28888b93168059fd242a1fc4438eb
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-01 05:31:58 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,67 +3,76 @@ import re
 import requests
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pytube import YouTube
+from pytube.cli import on_progress
 
 
 # Define your Telegram bot token
-TOKEN = '6162835664:AAF82yhi5W7jJe8VJxeLTk10xKGCLWBn6FkN'
+TOKEN = '6162835664:AAF82yhi5W7jJe8VJxeLTk10xKGCLWBn6Fk'
+
 
 # Define the command handler for the /start command
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the TikTok downloader bot!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the TikTok downloader bot! Send me a TikTok video URL to download.")
+
 
 # Define the function to handle text messages
 def handle_message(update, context):
     message_text = update.message.text
-    
+
     # Check if the message text is a valid TikTok video URL
     if 'vt.tiktok.com' in message_text:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading TikTok video...")
-        
+
         # Extract the TikTok video ID
         video_id = extract_video_id(message_text)
-        
+
         if video_id:
             # Generate the direct download URL for the TikTok video
             video_url = generate_direct_download_url(video_id)
-            
+
             # Download the TikTok video using requests library
             response = requests.get(video_url, stream=True)
-            
+
             # Get the filename from the URL
             video_filename = f"{video_id}.mp4"
-            
+
             # Save the video to a file
             with open(video_filename, 'wb') as file:
+                total_size = int(response.headers.get('content-length'))
+                downloaded_size = 0
                 for chunk in response.iter_content(chunk_size=1024):
                     if chunk:
                         file.write(chunk)
-            
+                        downloaded_size += len(chunk)
+                        progress = (downloaded_size / total_size) * 100
+                        context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")
+                        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Downloading video: {progress:.2f}%")
+
             # Send the downloaded video file
             context.bot.send_video(chat_id=update.effective_chat.id, video=open(video_filename, 'rb'), supports_streaming=True)
-            
+
             # Remove the downloaded video file
             os.remove(video_filename)
-            
+
             # Generate the direct download URL for the TikTok audio
             audio_url = generate_direct_audio_url(video_id)
-            
+
             if audio_url:
                 # Download the TikTok audio using pytube library
                 audio_filename = f"{video_id}.mp3"
-                YouTube(audio_url).streams.first().download(filename='audio', filename_prefix='')
+                YouTube(audio_url, on_progress_callback=on_progress).streams.first().download(filename='audio', filename_prefix='')
                 os.rename(audio_filename, audio_filename + '.mp3')
-                
+
                 # Send the downloaded audio file
                 context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(audio_filename + '.mp3', 'rb'))
-                
+
                 # Remove the downloaded audio file
                 os.remove(audio_filename + '.mp3')
-            
+
         else:
             # Send an error message if the video ID cannot be extracted
             context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to extract TikTok video ID.")
-            
+
     else:
         # Send an error message if the message is not a valid TikTok URL
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid TikTok video URL.")
